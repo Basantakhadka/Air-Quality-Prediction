7ce8ae1712a4f43d1305d8f9888b081df6e95908
Basantakhadka <46421489+Basantakhadka@users.noreply.github.com>
2020-09-18 23:23:16 +0545

    Add files via upload

--- airquality/airprediction/main/admin.py
+++ airquality/airprediction/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Dataset,Dataset1,Dataset2,Dataset3,whole_dataset
+from import_export.admin import ImportExportModelAdmin
+
+@admin.register(Dataset,Dataset1,Dataset2,Dataset3,whole_dataset)
+class ViewAdmin( ImportExportModelAdmin):
+	pass
--- airquality/airprediction/main/models.py
+++ airquality/airprediction/main/models.py
@@ -23,19 +23,19 @@ class Dataset3(models.Model):
 	Ozone=models.DecimalField(max_digits=10, decimal_places=6,blank=False)
 	Pm25=models.DecimalField(max_digits=10, decimal_places=6,blank=False,null=True)
 
-class book(models.Model):
+
+
+
+class whole_dataset(models.Model):
 	date=models.CharField(max_length=120)
 	location=models.TextField(max_length=120)
 	year=models.IntegerField(blank=False)
 	month=models.IntegerField(blank=False)
-	month=models.IntegerField(blank=False)
+	day=models.IntegerField(blank=False)
 	rainfall=models.DecimalField(max_digits=10,decimal_places=6,blank=False,null=True)
 	tmax=models.DecimalField(max_digits=10,decimal_places=6,blank=False,null=True)
 	tmin=models.DecimalField(max_digits=10,decimal_places=6,blank=False,null=True)
 	relative_humidity=models.DecimalField(max_digits=10,decimal_places=6,blank=False,null=True)
 	ozone=models.DecimalField(max_digits=10, decimal_places=6,blank=False)
 	Pm25=models.DecimalField(max_digits=10, decimal_places=6,blank=False,null=True)
-
-
-
 
